85910f7ecaf1e6caae3f09a7384d5bd5671a29d5
harsh <harshjoshihj3382565@gmail.com>
2023-05-07 16:41:59 +0530

    first commit

---
+++ components/Sixpart.js
@@ -0,0 +1,13 @@
+import './six.css'
+
+export default function Sixpart() {
+  return (
+    <main>
+      <div className="para2">
+        <img src="./images/six6.png" />
+        <h1>Ready to start your journey?</h1>
+        <button > <img src="./images/down.png"/> Download for windows</button>
+      </div>
+    </main>
+  )
+}
---
+++ components/Thirdpart.js
@@ -0,0 +1,17 @@
+import "./thirdpart.css"
+
+export default function Thirdpart() {
+  return (
+    <main className="thirdpart">
+      <div className="third3">
+         <h1>Where hanging out is easy</h1>
+        <p>Grab a seat in a voice channel when you’re</p>
+        <p>free. Friends in your server can see you’re</p>
+        <p>around and instantly pop in to talk without</p>
+        <p>having to call.</p> 
+        </div>
+              <img src="./images/pro.png" />
+    </main>
+  )
+}
+
